0d59073ef7dda7ba3e6317767c9824ef31142ca6
Kristof Menyhert <menyhert.kristof@gmail.com>
2023-05-02 23:26:49 +0200

    set up email

--- utils/b2b_contact_form.py
+++ utils/b2b_contact_form.py
@@ -35,7 +35,7 @@ def send_email(recipient_address, subject, body):
 
     # create a SMTP client session
     smtp_server = 'smtp.eu.mailgun.org'
-    smtp_port = 587
+    smtp_port = 465
     smtp_username = st.secrets['email']['smtp_username']
     smtp_password = st.secrets['email']['smtp_password']
     with smtplib.SMTP(smtp_server, smtp_port) as smtp:
